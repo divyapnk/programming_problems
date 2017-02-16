d0e775fcd1b1fcd6db05b46eb1967936c1b023dc
Divya P <divyapnk89@gmail.com>
2017-02-16 10:23:28 +0530

    Commit Anagrams

---
+++ programming_problems/src/com/learn/java/Anagram.java
@@ -0,0 +1,17 @@
+package com.learn.java;
+
+import java.util.Scanner;
+
+public class Anagram {
+
+	public static void main(String[] args) {		
+		Scanner scanner = new Scanner(System.in);
+		
+		String str1 = scanner.next();
+		String str2 = scanner.next();
+		
+		
+
+	}
+
+}
