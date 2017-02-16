91ec898ca0444482038a23cc7d066566d979a208
Divya P <divyapnk89@gmail.com>
2017-02-16 17:14:21 +0530

    Commit Anagram

--- programming_problems/src/com/learn/java/Anagram.java
+++ programming_problems/src/com/learn/java/Anagram.java
@@ -7,11 +7,39 @@ public class Anagram {
 	public static void main(String[] args) {		
 		Scanner scanner = new Scanner(System.in);
 		
-		String str1 = scanner.next();
-		String str2 = scanner.next();
+		String str1 = scanner.nextLine();
+		String str2 = scanner.nextLine();
 		
+		int len1 = str1.length();
+		int len2 = str2.length();
 		
-
+		int found = 0;
+		int not_found = 0;
+		
+		if(len1 == len2){
+			int len = len1;
+			for(int i=0; i<len; i++){
+				found = 0;
+				for(int j=0; j<len; j++){
+					if(str1.charAt(i) == str2.charAt(j)){
+						found=1;
+						break;
+					}
+				}
+				if(found==0){
+					not_found=1;
+					break;
+				}
+			}
+			if(not_found==1){
+				System.out.println("Strings are Not Anagrams to Each Other..!!");
+			}
+			else
+				System.out.println("Strings are Anagrams");
+		}
+		else
+        {
+            System.out.print("Both Strings Must have the same number of Character to be an Anagram");
+        }    
 	}
-
 }
